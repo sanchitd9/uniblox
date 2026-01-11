f5d85730a287a7c43ce0bacd8d68e1161b9362b0
Sanchit Dass <san.chit2640@gmail.com>
2026-01-11 18:24:05 +0530

    add checkout functionality

--- app.js
+++ app.js
@@ -7,8 +7,11 @@ const PORT = 3000;
 
 function loadProducts() {
   const products = JSON.parse(fs.readFileSync("data/products.json"));
-
   return products;
+}
+
+function calculateCartTotal() {
+  return store.cart.items.reduce((total, item) => total + (item.price * item.quantity), 0);
 }
 
 // Middleware
@@ -44,7 +47,8 @@ app.post("/api/cart/add", (req, res) => {
   const { productId, quantity } = req.body;
 
   const products = loadProducts();
-  const product = products.filter(p => p.id === productId);
+  const product = products.find(p => p.id === productId);
+  console.log(product);
 
   if (!product) {
     return res.status(404);
@@ -66,6 +70,31 @@ app.post("/api/cart/add", (req, res) => {
   return res.json(store.cart);
 });
 
+// Checkout
+app.post("/api/checkout", (req, res) => {
+  const { discountCode } = req.body;
 
+  store.orderCount++;
+
+  const totalAmount = calculateCartTotal();
+
+  // Create the order
+  const order = {
+    orderId: `ORDER${store.orderCount}`,
+    items: [...store.cart.items],
+    totalAmount
+  };
+
+  // Add it to the store
+  store.orders.push(order);
+
+  // Clear the cart
+  store.cart.items = [];
+
+  return res.json({
+    message: "Order placed successfully",
+    order
+  })
+});
 
 app.listen(PORT);
